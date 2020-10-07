e6062f0c9de4f68d7526c51fd351ba38e5c802b3
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-07 20:22:41 +0300

    update actions defined

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -1,5 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { Product } from '../product';
+// update the state according to these commands
 
 export const toggleProductCode = createAction(
 	'[Product] Toggle Product Code'
@@ -32,4 +33,19 @@ export const loadProductsFailure = createAction(
 
 export const clearCurrentProduct = createAction(
 	'[Product] Clear Current Product'
+);
+
+export const updateProduct = createAction(
+	'[Product] Update Product',
+	props<{ product: Product }>()
+);
+
+export const updateProductSuccess = createAction(
+	'[Product] Update Product Success',
+	props<{ product: Product }>()
+);
+
+export const updateProductFailure = createAction(
+	'[Product] Update Product Fail',
+	props<{ error: string }>()
 );
